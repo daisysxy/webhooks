237cde56775585ead9a4a5c9a6de52563c4205b6
sxy <474037140@163.com>
2018-09-18 09:33:44 +0800

    正式

---
+++ src/Webhooks.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Sxy\Webhooks;
+
+use Illuminate\Http\Request;
+
+class Webhooks
+{
+    public function printRunning()
+    {
+        echo 'running';
+    }
+
+    public function handle(Request $request)
+    {
+        dd(1);
+        $hmac_header = $request->header('X-Gogs-Signature');
+        $data = $request->getContent();
+
+        if ($this->verify_webhook($data, $hmac_header)) {
+            $basePath = base_path();
+            $cmd = "cd $basePath \n git pull 2>&1";
+            $outout = [];
+            exec($cmd, $outout);
+            logger()->debug($request->json('ref'));
+            logger()->debug(implode("\n", $outout));
+
+            return implode("\n", $outout);
+        } else {
+            logger()->debug('unauthed push event');
+        }
+    }
+
+    private function verify_webhook($data, $hmac_header)
+    {
+        $calculated_hmac = hash_hmac('sha256', $data, config('code.gogs_key'), false);
+        return ($hmac_header == $calculated_hmac);
+    }
+}
--- src/WebhooksServiceProvider.php
+++ src/WebhooksServiceProvider.php
@@ -8,13 +8,13 @@ class AdminServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        //
+        $this->loadRoutesFrom(__DIR__ . '/web.php');
     }
 
     public function register()
     {
-        $this->app->singleton('admin', function () {
-            return new Admin;
+        $this->app->singleton('webhooks', function () {
+            return new Webhooks;
         });
     }
 }
